a8fe62914f95a6f693d3460aca478082f9eb8208
bryanangjy <137162406+bryanangjy@users.noreply.github.com>
2024-08-28 14:08:27 +0800

    Viewing our expenses

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, flash, render_template, request, url_for, redirect
-# from . import db
-# from .models import Expenses
+from . import db
+from .models import Expenses
 from .forms import EditExpense
 from flask_login import current_user, login_required
 from datetime import date
@@ -20,8 +20,14 @@ def adding_new_expenses():
 @views.route('/expenses')
 # @login_required
 def show_expenses():
-    # insert code here
-    return render_template("expenses.html")
+    expenses_user = Expenses.query.all()
+
+    total_amount = 0
+    for expense in expenses_user:
+        total_amount += expense.amount
+
+    return render_template("expenses.html", expenses=expenses_user,
+                           name_user="current_user.name", total=round(total_amount, 2))
 
 
 # UPDATE
